b1be9a905b207528e23541cc5716961929fe7ebb
im186-2014 <im186-2014@L1-29>
2017-11-16 12:06:47 +0100

    fgjd

--- UserMS/DataAccess/UserDB.cs
+++ UserMS/DataAccess/UserDB.cs
@@ -4,35 +4,75 @@ using System.Linq;
 using System.Web;
 using UserMS.Models;
 using UserMS.Util;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace UserMS.DataAccess
 {
     public static class UserDB
     {
-        public static List<User> usersInMemory = new List<User>();
-
-
-        public static List<User> GetUsers
+        public static UserDB GetUserById(int id)
         {
-            return usersInMemory;
-        }
-
-        public static User GetUserById(int id)
-        {
-            foreach (User user in usersInMemory)
+            try
             {
-                if (user.Id == id)
+                User userToReturn = new User();
+
+                using (SqlConnection connection = new SqlConnection(DBFunctions.ConnectionString))
                 {
-                    return user;
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandTex = String.Format(@"
+                        SELECT *
+                        FROM [user].[User]
+                        WHERE [id] = @id
+                    ");
+                    command.Parameters.Add("@id", SqlDbType.Int);
+                    command.Parameters["@Id"].Value = id;
+                    using (SqlDataReader reader = command.ExecuteReader())
+
+                    {
+                        if (reader.Read())
+                        {
+                            userToReturn.Id = (int)reader["Id"];
+                            userToReturn.Name = reader["Name"] as string; 
+                        }
+
+                    }
                 }
+                return userToReturn;
+
             }
-            return null;
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
         }
 
-        public static User CreateUser(User user)
-        {
-            usersInMemory.Add(user);
-            return GetUserById(user.Id.Value);
-        }
+        //public static List<User> usersInMemory = new List<User>();
+
+
+        //public static List<User> GetUsers
+        //{
+        //    return usersInMemory;
+        //}
+
+        //public static User GetUserById(int id)
+        //{
+        //    foreach (User user in usersInMemory)
+        //    {
+        //        if (user.Id == id)
+        //        {
+        //            return user;
+        //        }
+        //    }
+        //    return null;
+        //}
+
+        //public static User CreateUser(User user)
+        //{
+        //    usersInMemory.Add(user);
+        //    return GetUserById(user.Id.Value);
+        //}
+
     }
 }
---
+++ UserMS/Util/DBFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using.System.Configuration;
+
+namespace UserMS.Util
+{
+    public class DBFunctions
+    {
+        public static string ConnectionString 
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionString["URIS_IIM_DBConnection"].ConnectionString;
+            }
+        }
+
+    }
+}
